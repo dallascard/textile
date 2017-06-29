a5a5770ef728789b75bee60e58ec2cdd4a5664b1
dallascard <dcard@andrew.cmu.edu>
2017-06-29 15:55:48 -0700

    testing part of viz

--- core/vizualization/plot_propotions_by_metadata.py
+++ core/vizualization/plot_propotions_by_metadata.py
@@ -1,8 +1,8 @@
 import os
 from optparse import OptionParser
-from collections import Counter
 
-import matplotlib.pyplot as plt
+import numpy as np
+#import matplotlib.pyplot as plt
 
 from ..util import dirs
 from ..util import file_handling as fh
@@ -33,13 +33,22 @@ def main():
     field_vals = list(set([data[k][field_name] for k in data.keys()]))
     field_vals.sort()
 
-    label_set = set([data[k][label] for k in data.keys()])
+    label_set = list(set([data[k][label] for k in data.keys()]))
+    label_set.sort()
+    n_labels = len(label_set)
+    label_vals = range(n_labels)
+    label_index = dict(zip(label_set, label_vals))
 
-    for val in field_vals:
+    proportions = np.zeros([len(field_vals), len(label_vals)])
+
+    for v_i, val in enumerate(field_vals):
         print(val)
         subset = {k: v for k, v in data.items() if data[k][field_name] == val}
-        counter = Counter()
-        counter.update([subset[k][label] for k in subset.keys()])
+        label_counts = np.bincount([label_index[subset[k][label]] for k in subset.keys()], minlength=n_labels)
+        label_props = label_counts / float(len(subset))
+        proportions[v_i, :] = label_props
+
+    print(proportions)
 
 if __name__ == '__main__':
     main()
