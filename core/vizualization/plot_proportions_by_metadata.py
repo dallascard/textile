9668dbbde423fd66e7b86fd49d4f9f2cc589b7bd
dallascard <dcard@andrew.cmu.edu>
2017-06-29 15:59:07 -0700

    adding more info

--- core/vizualization/plot_proportions_by_metadata.py
+++ core/vizualization/plot_proportions_by_metadata.py
@@ -26,29 +26,32 @@ def main():
     if n_classes is not None:
         n_classes = int(n_classes)
 
-    data_dir = dirs.dir_data_raw(project)
     datafile = os.path.join(dirs.dir_data_raw(project), subset + '.json')
-
     data = fh.read_json(datafile)
+
     field_vals = list(set([data[k][field_name] for k in data.keys()]))
     field_vals.sort()
 
     label_set = list(set([data[k][label] for k in data.keys()]))
     label_set.sort()
-    n_labels = len(label_set)
-    label_vals = range(n_labels)
+    if n_classes is None:
+        n_classes = len(label_set)
+    label_vals = range(n_classes)
     label_index = dict(zip(label_set, label_vals))
 
     proportions = np.zeros([len(field_vals), len(label_vals)])
+    n_stories = []
 
     for v_i, val in enumerate(field_vals):
         print(val)
         subset = {k: v for k, v in data.items() if data[k][field_name] == val}
-        label_counts = np.bincount([label_index[subset[k][label]] for k in subset.keys()], minlength=n_labels)
+        n_stories.append(len(subset))
+        label_counts = np.bincount([label_index[subset[k][label]] for k in subset.keys()], minlength=n_classes)
         label_props = label_counts / float(len(subset))
         proportions[v_i, :] = label_props
 
     print(proportions)
+    print(n_stories)
 
 if __name__ == '__main__':
     main()
