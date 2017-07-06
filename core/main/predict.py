c94804c72ce381cd96cd607dc1a7b09b4db5a610
dallascard <dcard@andrew.cmu.edu>
2017-07-06 15:04:15 -0700

    fixed loading model for prediction

--- core/main/predict.py
+++ core/main/predict.py
@@ -7,15 +7,15 @@ import pandas as pd
 
 from ..util import dirs
 from ..util import file_handling as fh
-from ..models import lr, blr
+from ..models import lr, blr, load_model
 from ..preprocessing import features
 
 
 def main():
     usage = "%prog project_dir predict_subset model_name"
     parser = OptionParser(usage=usage)
-    parser.add_option('--model', dest='model', default='LR',
-                      help='Model type [LR|BLR]: default=%default')
+    parser.add_option('--model', dest='model', default=None,
+                      help='Model type [LR|BLR]; None=auto-detect: default=%default')
     parser.add_option('--label', dest='label', default='label',
                       help='Label name: default=%default')
     #parser.add_option('--keyword', dest='key', default=None,
@@ -28,21 +28,17 @@ def main():
     predict_subset = args[1]
     model_name = args[2]
 
-    # TODO: make this automatic
-    model_type = options.mmodel
+    model_type = options.model
     label = options.label
+
     load_and_predict(project_dir, model_type, model_name, predict_subset, label)
 
 
 def load_and_predict(project_dir, model_type, model_name, test_subset, label_name):
     print("Loading model")
+
     model_dir = os.path.join(dirs.dir_models(project_dir), model_name)
-    if model_type == 'LR':
-        model = lr.load_from_file(model_dir)
-    elif model_type == 'BLR':
-        model = blr.load_from_file(model_dir)
-    else:
-        sys.exit("Model type not recognized")
+    model = load_model.load_model(model_dir, model_type)
 
     feature_signatures = fh.read_json(os.path.join(model_dir, 'features.json'))
     test_features_dir = dirs.dir_features(project_dir, test_subset)
