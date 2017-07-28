fdbcaeb6a7b1cd443fadffd2db6667c9748704c5
dallascard <dcard@andrew.cmu.edu>
2017-07-28 13:39:30 -0700

    debug

--- core/plotting/summarize_cross_eval_results.py
+++ core/plotting/summarize_cross_eval_results.py
@@ -65,6 +65,7 @@ def main():
             best_counts_df['best'] += errors[1:] <= np.min(errors[1:])
             worst_counts_df['worst'] += errors[1:] >= np.max(errors[1:])
             order = np.argsort(errors[1:])
+            print(v, f, order)
             ranking_df['rank'] += order / float(n_output_files)
 
         mean_rmse_df.loc[v] = errors_df.mean(axis=0)
