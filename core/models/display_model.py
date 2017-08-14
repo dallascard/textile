ecd8200c47c66ed554f08a7230313a4dfe1beed7
dallascard <dcard@andrew.cmu.edu>
2017-08-14 16:43:08 -0700

    removed loop

--- core/models/display_model.py
+++ core/models/display_model.py
@@ -51,16 +51,9 @@ def main():
         word_vectors_prefix = features[0]['word_vectors_prefix']
         word_vectors = fh.load_dense(word_vectors_prefix + '.npz')
         word_vector_terms = fh.read_json(word_vectors_prefix + '.json')
-        vocab_size, dv = word_vectors.shape
         n_classes = model.get_n_classes()
-        activations = np.zeros([vocab_size, n_classes])
 
-        for i in range(vocab_size):
-            if i % 1000 == 0 and i > 0:
-                print(i)
-            X = word_vectors[i, :].reshape((1, dv))
-            probs = model.predict_probs(X)
-            activations[i, :] = probs
+        activations = model.predict_probs(word_vectors)
 
         for cl in range(n_classes):
             order = np.argsort(activations[:, cl]).tolist()
