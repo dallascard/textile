fd6238dcbb9a95725ef46bcd5dea291986eee25b
dallascard <dcard@andrew.cmu.edu>
2017-09-13 22:16:32 -0700

    updated preprocessing script

--- do_preprocessing.py
+++ do_preprocessing.py
@@ -8,26 +8,27 @@ from core.preprocessing import preprocess_word_vectors
 
 
 def main():
-    usage = "%prog project subset word2vec_file"
+    usage = "%prog project word2vec_file"
     parser = OptionParser(usage=usage)
-    #parser.add_option('--ref', dest='ref', default='unigrams',
+    #parser.add_option('--label', dest='label', default='label',
     #                  help='Reference feature definition: default=%default')
     #parser.add_option('-s', dest='size', default=300,
     #                  help='Size of word vectors: default=%default')
 
     (options, args) = parser.parse_args()
     project = args[0]
-    subset = args[1]
-    word2vec_file = args[2]
+    word2vec_file = args[1]
 
     #subset = 'pro_tone'
     #base_project = os.path.join('projects', 'mfc')
     #subprojects = ['climate', 'guncontrol', 'immigration', 'samesex', 'smoking']
 
-    preprocess_labels.preprocess_labels(project, subset, label_name='label', metadata_fields=['year_group'])
-    preprocess_words.preprocess_words(project, subset, lower=True)
-    preprocess_words.preprocess_words(project, subset, ngrams=1, lower=False, suffix='_default')
-    preprocess_word_vectors.preprocess_word_vectors(project, subset, word2vec_file, ref='unigrams_default')
+    pairs = [('framing', 'Economic'), ('framing', 'Legality'), ('framing', 'Health'), ('framing', 'Political')]
+    for subset, label in pairs:
+        preprocess_labels.preprocess_labels(project, subset, label_name=label, metadata_fields=['year_group'])
+        preprocess_words.preprocess_words(project, subset, lower=True)
+        preprocess_words.preprocess_words(project, subset, ngrams=1, lower=False, suffix='_default')
+        preprocess_word_vectors.preprocess_word_vectors(project, subset, word2vec_file, ref='unigrams_default')
 
 if __name__ == '__main__':
     main()
