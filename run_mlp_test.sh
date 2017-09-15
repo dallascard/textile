6ee22124326337c724380ce6b2282202d3c42da8
dallascard <dcard@andrew.cmu.edu>
2017-09-15 16:49:52 -0700

    modifying scripts

--- run_mlp_test.sh
+++
@@ -1,3 +0,0 @@
-OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/immigration/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r0
-OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/immigration/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r1
-OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/immigration/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r2
---
+++ run_mlp_tone.sh
@@ -0,0 +1,16 @@
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/climate/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r0
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/climate/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r1
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/climate/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r2
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/guncontrol/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r0
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/guncontrol/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r1
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/guncontrol/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r2
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/immigration/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r0
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/immigration/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r1
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/immigration/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r2
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/samesex/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r0
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/samesex/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r1
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/samesex/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r2
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/smoking/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r0
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/smoking/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r1
+OMP_NUM_THREADS=4 python -m core.experiments.combo projects/mfc/smoking/ pro_tone year_group config/doc2vec_test.json --model MLP -t 0.9 --n_calib 100 --penalty l2 --suffix _r2
+
