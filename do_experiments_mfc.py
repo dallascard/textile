ff7dbdce1683952182f13f63ae80b2136979c290
dallascard <dcard@andrew.cmu.edu>
2017-09-16 14:16:35 -0700

    script update

--- do_experiments_mfc.py
+++ do_experiments_mfc.py
@@ -11,10 +11,10 @@ def main():
                       help='Field to split on: default=%default')
     parser.add_option('--config', dest='config', default='default.json',
                       help='Field to split on: default=%default')
-    parser.add_option('-p', dest='calib_prop', default=0.1,
-                      help='Proportion of test data to use for calibration: default=%default')
     parser.add_option('-t', dest='train_prop', default=0.9,
                       help='Proportion of training data to use for training: default=%default')
+    parser.add_option('--n_calib', dest='n_calib', default=100,
+                      help='Number of test instances to use for calibration: default=%default')
     parser.add_option('--penalty', dest='penalty', default='l2',
                       help='Regularization type: default=%default')
     parser.add_option('-r', dest='repeats', default=5,
@@ -28,8 +28,8 @@ def main():
 
     field_name = options.field_name
     config_file = os.path.join('config', options.config)
-    calib_prop = float(options.calib_prop)
     train_prop = float(options.train_prop)
+    n_calib = int(options.n_calib)
     penalty = options.penalty
     repeats = int(options.repeats)
     model_type = options.model
@@ -38,16 +38,16 @@ def main():
 
     for subset, label in pairs:
         print("\n\nStarting", subset, label)
-        combo.cross_train_and_eval(project, subset, field_name, config_file, calib_prop, train_prop, suffix='',
+        combo.cross_train_and_eval(project, subset, field_name, config_file, n_calib, train_prop, suffix='',
                                    model_type=model_type, label=label, penalty=penalty, repeats=repeats,
                                    objective='f1')
-        combo.cross_train_and_eval(project, subset, field_name, config_file, calib_prop, train_prop, suffix='',
+        combo.cross_train_and_eval(project, subset, field_name, config_file, n_calib, train_prop, suffix='',
                                    model_type=model_type, label=label, penalty=penalty, repeats=repeats,
                                    objective='calibration')
-        combo.cross_train_and_eval(project, subset, field_name, config_file, calib_prop, train_prop, suffix='',
+        combo.cross_train_and_eval(project, subset, field_name, config_file, n_calib, train_prop, suffix='',
                                    model_type=model_type, label=label, penalty=penalty, repeats=repeats,
                                    objective='f1', cshift='classify')
-        combo.cross_train_and_eval(project, subset, field_name, config_file, calib_prop, train_prop, suffix='',
+        combo.cross_train_and_eval(project, subset, field_name, config_file, n_calib, train_prop, suffix='',
                                    model_type=model_type, label=label, penalty=penalty, repeats=repeats,
                                    objective='calibration', cshift='classify')
 
