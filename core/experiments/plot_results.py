e7a48f2a3cb6fa4bed88ce76360b21b1c0fcc0d6
dallascard <dcard@andrew.cmu.edu>
2017-09-19 10:18:15 -0400

    testing

--- core/experiments/plot_results.py
+++ core/experiments/plot_results.py
@@ -45,92 +45,93 @@ def main():
 
     # basic LR f1: combining subset, label, repetitions, and pre/post date
     #basename = '*_' + model_type
-    #for objective in ['f1', 'calibration']:
-    basename = '*_' + model_type + '_' + penalty
-    if model_type == 'MLP':
-        basename += '_' + dh
-    #basename += '_' + train_prop + '_' + calib_prop + '_' + objective
-    #basename += '_*_' + objective
-    basename += '_*_' + n_calib + '_' + objective
-    if cshift is not None:
-        basename += '_cshift'
-    if base == 'mfc':
-        basename += '_*_????_?'
-    elif base == 'amazon':
-        basename += '_????_?'
-
-    print(basename)
-    search_string = os.path.join('projects', base, subset, 'models', basename, 'results.csv')
-    print(search_string)
-    files = glob(search_string)
-    files.sort()
-    n_files = len(files)
-
-    train_props = []
-    for f in files:
-        match = re.match(r'.*' + penalty + r'_([0-9]+\.[0-9]+)_.*', f)
-        train_props.append(match.group(1))
-
-    train_props = list(set(train_props))
-    print(train_props)
-
-    n_train = []
-    CC_nontrain = []
-    PCC_nontrain = []
-    n_train_means = [0]
-    CC_means = [0]
-    PCC_means = [0]
-    for t in train_props:
+    for objective in ['f1', 'calibration']:
         basename = '*_' + model_type + '_' + penalty
         if model_type == 'MLP':
             basename += '_' + dh
-        basename += '_' + t + '_' + n_calib + '_' + objective
+        #basename += '_' + train_prop + '_' + calib_prop + '_' + objective
+        #basename += '_*_' + objective
+        basename += '_*_' + n_calib + '_' + objective
         if cshift is not None:
             basename += '_cshift'
         if base == 'mfc':
-            basename += '_???_????_?'
+            basename += '_*_????_?'
         elif base == 'amazon':
             basename += '_????_?'
 
         print(basename)
-        files = glob(os.path.join('projects', base, subset, 'models', basename, 'results.csv'))
+        search_string = os.path.join('projects', base, subset, 'models', basename, 'results.csv')
+        print(search_string)
+        files = glob(search_string)
         files.sort()
         n_files = len(files)
 
-        print(files[0])
-        results = fh.read_csv_to_df(files[0])
-        df = pd.DataFrame(results[['N', 'estimate', 'RMSE', 'contains_test']].copy())
-        mean_df = pd.DataFrame(results[['N', 'estimate', 'RMSE', 'contains_test']].copy())
-        #n_train.append(float(t))
-        n_train.append(df.loc['train', 'N'])
-        CC_nontrain.append(df.loc['CC_nontrain', 'RMSE'])
-        PCC_nontrain.append(df.loc['PCC_nontrain', 'RMSE'])
+        train_props = []
+        for f in files:
+            match = re.match(r'.*' + penalty + r'_([0-9]+\.[0-9]+)_.*', f)
+            train_props.append(match.group(1))
 
-        for f in files[1:]:
-            print(f)
-            results = fh.read_csv_to_df(f)
-            df = results[['N', 'estimate', 'RMSE', 'contains_test']]
-            mean_df += results[['N', 'estimate', 'RMSE', 'contains_test']]
+        train_props = list(set(train_props))
+        print(train_props)
+
+        n_train = []
+        CC_nontrain = []
+        PCC_nontrain = []
+        n_train_means = [0]
+        CC_means = [0]
+        PCC_means = [0]
+        for t in train_props:
+            basename = '*_' + model_type + '_' + penalty
+            if model_type == 'MLP':
+                basename += '_' + dh
+            basename += '_' + t + '_' + n_calib + '_' + objective
+            if cshift is not None:
+                basename += '_cshift'
+            if base == 'mfc':
+                basename += '_???_????_?'
+            elif base == 'amazon':
+                basename += '_????_?'
+
+            print(basename)
+            files = glob(os.path.join('projects', base, subset, 'models', basename, 'results.csv'))
+            files.sort()
+            n_files = len(files)
+
+            print(files[0])
+            results = fh.read_csv_to_df(files[0])
+            df = pd.DataFrame(results[['N', 'estimate', 'RMSE', 'contains_test']].copy())
+            mean_df = pd.DataFrame(results[['N', 'estimate', 'RMSE', 'contains_test']].copy())
             #n_train.append(float(t))
             n_train.append(df.loc['train', 'N'])
             CC_nontrain.append(df.loc['CC_nontrain', 'RMSE'])
             PCC_nontrain.append(df.loc['PCC_nontrain', 'RMSE'])
 
-        mean_df = mean_df / float(n_files)
+            for f in files[1:]:
+                print(f)
+                results = fh.read_csv_to_df(f)
+                df = results[['N', 'estimate', 'RMSE', 'contains_test']]
+                mean_df += results[['N', 'estimate', 'RMSE', 'contains_test']]
+                #n_train.append(float(t))
+                n_train.append(df.loc['train', 'N'])
+                CC_nontrain.append(df.loc['CC_nontrain', 'RMSE'])
+                PCC_nontrain.append(df.loc['PCC_nontrain', 'RMSE'])
 
-        #n_train_means.append(float(t))
-        n_train_means.append(mean_df.loc['train', 'N'])
-        CC_means.append(mean_df.loc['CC_nontrain', 'RMSE'])
-        PCC_means.append(mean_df.loc['PCC_nontrain', 'RMSE'])
+            mean_df = mean_df / float(n_files)
 
-    print(n_train_means)
-    print(CC_means)
-    print(PCC_means)
-    plt.scatter(n_train, CC_nontrain)
-    plt.scatter(n_train, PCC_nontrain, alpha=0.5)
-    plt.scatter(n_train_means, CC_means)
-    plt.scatter(n_train_means, PCC_means, alpha=0.5)
-    #plt.plot(np.array(n_train_means), np.array(PCC_means), alpha=0.5, label=objective)
+            #n_train_means.append(float(t))
+            n_train_means.append(mean_df.loc['train', 'N'])
+            CC_means.append(mean_df.loc['CC_nontrain', 'RMSE'])
+            PCC_means.append(mean_df.loc['PCC_nontrain', 'RMSE'])
+
+        print(n_train_means)
+        print(CC_means)
+        print(PCC_means)
+        plt.scatter(n_train, CC_nontrain)
+        plt.scatter(n_train, PCC_nontrain, alpha=0.5)
+        plt.scatter(n_train_means, CC_means)
+        plt.scatter(n_train_means, PCC_means, alpha=0.5)
+        #plt.plot(np.array(n_train_means), np.array(PCC_means), alpha=0.5, label=objective)
+
     plt.legend()
     plt.savefig('test.pdf')
     #plt.show()
