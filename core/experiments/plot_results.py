04586e88cadf38594c3c3437a35a2ff02fd6292e
dallascard <dcard@andrew.cmu.edu>
2017-09-19 10:16:19 -0400

    testing

--- core/experiments/plot_results.py
+++ core/experiments/plot_results.py
@@ -68,9 +68,7 @@ def main():
 
     train_props = []
     for f in files:
-        print(f)
         match = re.match(r'.*' + penalty + r'_([0-9]+\.[0-9]+)_.*', f)
-        print(match.group(1))
         train_props.append(match.group(1))
 
     train_props = list(set(train_props))
