0d9dd57a50ead4d8caa0ed468fecff977b415504
dallascard <dcard@andrew.cmu.edu>
2017-09-22 13:13:26 -0700

    trying something with venn error

--- core/experiments/combine_results.py
+++ core/experiments/combine_results.py
@@ -80,6 +80,8 @@ def main():
     venn_outside_error = 0
     n_outside = 0
 
+    print(venn_av_lower, target_prop, venn_av_upper)
+
     if not (venn_av_lower < target_prop < venn_av_upper):
         venn_outside_error += max(venn_av_lower - target_prop, target_prop - venn_av_upper)
         n_outside += 1
@@ -95,6 +97,8 @@ def main():
         venn_av_upper = results.loc['Venn_averaged', '95ucl']
         venn_outside_error = 0
         n_outside = 0
+
+        print(venn_av_lower, target_prop, venn_av_upper)
 
         if not (venn_av_lower < target_prop < venn_av_upper):
             venn_outside_error += max(venn_av_lower - target_prop, target_prop - venn_av_upper)
