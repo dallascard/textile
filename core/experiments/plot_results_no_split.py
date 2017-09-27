950441f74a4818eba77dae94da5d48fd21acd849
dallascard <dcard@andrew.cmu.edu>
2017-09-27 12:27:36 -0700

    adding erros

--- core/experiments/plot_results_no_split.py
+++ core/experiments/plot_results_no_split.py
@@ -169,19 +169,19 @@ def main():
         dot_size = 5
         linewidth=2
         if objective == 'f1':
-            ax.scatter(np.array(x)-24, ACC_nontrain, c=CB6[0], alpha=0.5, s=dot_size)
+            ax.scatter(np.array(x)-36, ACC_nontrain, c=CB6[0], alpha=0.5, s=dot_size)
             ax.plot(n_train_means, ACC_means, label='ACC', c=CB6[0], linewidth=linewidth)
 
-            ax.scatter(np.array(x)-24, CC_nontrain, c=CB6[1], alpha=0.5, s=dot_size)
+            ax.scatter(np.array(x)-18, CC_nontrain, c=CB6[1], alpha=0.5, s=dot_size)
             ax.plot(n_train_means, CC_means, label='CC', c=CB6[1], linewidth=linewidth)
             #ax.plot(n_train_means, np.array(CC_means) + np.array(CC_stds), linestyle='dashed', c=colors[0], label='CC (+1std)', alpha=0.5)
 
         #ax.scatter(x, PCC_nontrain, c=colors[1], alpha=0.5, s=10)
         if objective == 'f1':
-            ax.scatter(np.array(x)-8, PCC_nontrain, c=CB6[2], alpha=0.5, s=dot_size)
+            ax.scatter(np.array(x), PCC_nontrain, c=CB6[2], alpha=0.5, s=dot_size)
             ax.plot(n_train_means, PCC_means, label='PCC (acc)', c=CB6[2], linewidth=linewidth)
         else:
-            ax.scatter(np.array(x)+8, PCC_nontrain, c=CB6[3], alpha=0.5, s=dot_size)
+            ax.scatter(np.array(x)+18, PCC_nontrain, c=CB6[3], alpha=0.5, s=dot_size)
             ax.plot(n_train_means, PCC_means, label='PCC (cal)', c=CB6[3], linewidth=linewidth)
         #ax.plot(n_train_means, np.array(PCC_means) + np.array(PCC_stds), label=name + ' (+1std)', linestyle='dashed', alpha=0.5, c=colors[1])
         #ax.plot(n_train_means, PCC_maxes, label=name + ' (max)', linestyle='dashed', alpha=0.5)
@@ -189,7 +189,7 @@ def main():
         #ax.plot(n_train_means, Venn_means,  label='Venn' + objective[:3], alpha=0.5)
 
         if objective == 'calibration':
-            ax.scatter(np.array(x)+24, SRS, c=CB6[4], alpha=0.5, s=dot_size)
+            ax.scatter(np.array(x)+36, SRS, c=CB6[4], alpha=0.5, s=dot_size)
             ax.plot(n_train_means, SRS_means,  label='SRS', c=CB6[4], linewidth=linewidth)
             #ax.plot(n_train_means, np.array(SRS_means) + np.array(SRS_stds), label='SRS' + ' (+1std)', linestyle='dashed', alpha=0.5, c=colors[2])
 
