9c1eacc4a34bef5cfdd283e5aa8cd7e835c9a0cf
dallascard <dcard@andrew.cmu.edu>
2017-09-28 15:54:00 -0700

    offset

--- core/experiments/combine_results.py
+++ core/experiments/combine_results.py
@@ -137,7 +137,7 @@ def main():
         venn_outside_errors.append(max(venn_av_lower - target_prop, target_prop - venn_av_upper))
         n_outside += 1
 
-    diffs_bw_train_and_test.append(np.abs(results.loc['train', 'estimate'] -results.loc['target', 'estimate']))
+    diffs_bw_train_and_test.append(abs(results.loc['train', 'estimate'] -results.loc['target', 'estimate']))
     train_estmates.append(results.loc['train', 'estimate'])
     PCC_estimates.append(results.loc['PCC_nontrain', 'estimate'])
     train_errors.append(results.loc['target', 'estimate'] - results.loc['train', 'estimate'])
@@ -194,7 +194,7 @@ def main():
             venn_outside_errors.append(max(venn_av_lower - target_prop, target_prop - venn_av_upper))
             n_outside += 1
 
-        diffs_bw_train_and_test.append(np.abs(results.loc['train', 'estimate'] - results.loc['target', 'estimate']))
+        diffs_bw_train_and_test.append(abs(results.loc['train', 'estimate'] - results.loc['target', 'estimate']))
         target_estimates.append(target_estimate)
         PCC_errors.append(results.loc['target', 'estimate'] - results.loc['PCC_nontrain', 'estimate'])
         train_errors.append(results.loc['target', 'estimate'] - results.loc['train', 'estimate'])
