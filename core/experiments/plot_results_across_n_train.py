69b65eb39c378c1502f029a61e9249d732628146
dallascard <dcard@andrew.cmu.edu>
2017-09-28 16:56:21 -0700

    offset

--- core/experiments/plot_results_across_n_train.py
+++ core/experiments/plot_results_across_n_train.py
@@ -215,7 +215,7 @@ def main():
             ax.scatter(np.array(x)+1.5*offset, PCC_nontrain, c=CB6[3], alpha=0.5, s=dot_size)
             ax.plot(n_train_means, PCC_means, label='PCC (cal)', c=CB6[3], linewidth=linewidth)
 
-        if objective == 'f1':
+        if objective == 'calibration':
             ax.plot(n_train_means, train_means, 'k--', label='Train', alpha=0.5, linewidth=1)
         #if objective == 'f1':
         #    ax.scatter(np.array(x)+2.5*offset, Venn, c=CB6[5], alpha=0.5, s=dot_size)
