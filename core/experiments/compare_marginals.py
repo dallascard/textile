80ba57b26c9848b4fd1243bb6ce62a5cd00a896f
dallascard <dcard@andrew.cmu.edu>
2017-10-05 09:49:53 -0700

    debug

--- core/experiments/compare_marginals.py
+++ core/experiments/compare_marginals.py
@@ -209,7 +209,7 @@ def compare_marginals(project_dir, subset, label, field_name, feature_defs, item
             if np.sum(Y_train[i, :]) > 0:
                 ps_i = Y_train[i, :] / np.sum(Y_train[i, :])
                 p = ps_i[1]
-                vector = np.array(X_train[i, indices].todense())
+                vector = np.array(X_train[i, indices].todense()).ravel()
                 key = ''.join([str(int(s)) for s in vector])
                 train_counts[key] += 1
                 positives[key].append(p)
