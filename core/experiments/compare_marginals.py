7f100b4763db1306c1030ae348575d63d1bf93d7
dallascard <dcard@andrew.cmu.edu>
2017-10-05 11:40:32 -0700

    debug

--- core/experiments/compare_marginals.py
+++ core/experiments/compare_marginals.py
@@ -193,8 +193,8 @@ def compare_marginals(project_dir, subset, label, field_name, feature_defs, item
 
         n_nontrain, _ = X_nontrain.shape
 
-        #target_words = ['amendment', 'attorney', 'ban', 'benefits', 'case', 'civil', 'constitution', 'constitutional', 'court', 'courts', 'decision', 'decisions', 'federal', 'filed', 'granted', 'judge', 'judges', 'judicial', 'law', 'laws', 'lawsuit', 'lawyer', 'lawyers', 'legal', 'legalized', 'legality', 'licenses', 'majority', 'order', 'prop', 'protections', 'right', 'ruled', 'ruling', 'senate', 'suit', 'sued', 'supreme', 'unconstitutional']
-        target_words = ['court', 'law', 'judge', 'legal']
+        target_words = ['amendment', 'attorney', 'ban', 'benefits', 'case', 'civil', 'constitution', 'constitutional', 'court', 'courts', 'decision', 'decisions', 'federal', 'filed', 'granted', 'judge', 'judges', 'judicial', 'law', 'laws', 'lawsuit', 'lawyer', 'lawyers', 'legal', 'legalized', 'legality', 'licenses', 'majority', 'order', 'prop', 'protections', 'right', 'ruled', 'ruling', 'senate', 'suit', 'sued', 'supreme', 'unconstitutional']
+        #target_words = ['court', 'law', 'judge', 'legal']
         print("n words = ", len(target_words))
 
         indices = [col_names.index(w) for w in target_words]
