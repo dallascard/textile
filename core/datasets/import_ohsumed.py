82f0ae3db3536acb3549c3ffe5a79b5707dd8671
dallascard <dcard@andrew.cmu.edu>
2017-11-08 14:49:51 -0800

    preprocessing

--- core/datasets/import_ohsumed.py
+++ core/datasets/import_ohsumed.py
@@ -26,122 +26,84 @@ def main():
     category_counts = Counter()
     data = {}
 
-    key = None
-    field = None
-    text = ''
-    type = ''
-    title = ''
-    terms = None
-    count = 0
-    for line in train_lines:
-        if line.startswith('.I'):
-            # save the current article
-            if key is not None and terms is not None:
-                data[key] = {'text': title + '\n\n' + text, 'type': type, 'year': 1987}
-                for term in terms:
-                    data[key][term] = {0: 0, 1: 1}
-            # go on to the next article
-            key = line.split()[1].strip()
-            text = ''
-            count += 1
-            if count % 10000 == 0:
-                print(count)
-        elif line.startswith('.U'):
-            field = None
-        elif line.startswith('.S'):
-            field = None
-        elif line.startswith('.M'):
-            field = 'terms'
-        elif line.startswith('.T'):
-            field = 'title'
-        elif line.startswith('.P'):
-            field = 'type'
-        elif line.startswith('.W'):
-            field = 'text'
-        elif line.startswith('.A'):
-            field = None
-        elif field == 'terms':
-            terms = line.strip().split(';')
-            terms = [term.strip() for term in terms]
-            for term in terms:
-                if term not in categories:
-                    categories[term] = len(categories)
-            category_counts.update(terms)
-        elif field == 'title':
-            title = line.strip()
-        elif field == 'type':
-            type = line.strip()
-        elif field == 'text':
-            if text == '':
-                text = line.strip()
-            else:
-                text += '\n\n' + line.strip()
+    data, categories, category_counts = parse_data(train_lines, data, categories, category_counts, update_counts=True)
+    print("Total training articles = %d" % len(data))
 
-    key = None
-    field = None
-    text = ''
-    type = ''
-    title = ''
-    terms = None
-    count = 0
-    for line in test_lines:
-        if line.startswith('.I'):
-            if key is not None and terms is not None:
-                data[key] = {'text': title + '\n\n' + text, 'type': type, 'year': 1987}
-                for term in terms:
-                    data[key][term] = {0: 0, 1: 1}
-            # go on to the next article
-            key = line.split()[1].strip()
-            text = ''
-            count += 1
-            if count % 10000 == 0:
-                print(count)
-        elif line.startswith('.U'):
-            field = None
-        elif line.startswith('.S'):
-            field = None
-        elif line.startswith('.M'):
-            field = 'terms'
-        elif line.startswith('.T'):
-            field = 'title'
-        elif line.startswith('.P'):
-            field = 'type'
-        elif line.startswith('.W'):
-            field = 'text'
-        elif line.startswith('.A'):
-            field = None
-        elif field == 'terms':
-            terms = line.strip().split(';')
-            terms = [term.strip() for term in terms]
-            for term in terms:
-                if term not in categories:
-                    categories[term] = len(categories)
-        elif field == 'title':
-            title = line.strip()
-        elif field == 'type':
-            type = line.strip()
-        elif field == 'text':
-            if text == '':
-                text = line.strip()
-            else:
-                text += '\n\n' + line.strip()
-
+    data, categories, category_counts = parse_data(train_lines, data, categories, category_counts, update_counts=False)
     print("Total articles = %d" % len(data))
 
     most_common = category_counts.most_common(n=100)
     for t, c in most_common:
         print(t, c)
 
+    """
     for k_i, key in enumerate(data):
         for category in categories:
             if category not in data[key]:
                 data[key][category] = {0: 1, 1: 0}
         if k_i % 10000 == 0:
             print(k_i)
-
+    """
 
     fh.makedirs(dirs.dir_data_raw(project))
-    fh.write_to_json(data, os.path.join(dirs.dir_data_raw(project), 'all.json'))
+    fh.write_to_json(data, os.path.join(dirs.dir_data_raw(project), 'all.json'), sort_keys=True)
+    fh.write_to_json(most_common, os.path.join(dirs.dir_data_raw(project), 'most_common_categories.json'), sort_keys=False)
+
+
+def parse_data(lines, data, categories, category_counts, update_counts=False):
+    key = None
+    field = None
+    text = ''
+    type = ''
+    title = ''
+    terms = None
+    count = 0
+    for line in lines:
+        if line.startswith('.I'):
+            # save the current article
+            if key is not None and terms is not None:
+                data[key] = {'text': title + '\n\n' + text, 'type': type, 'year': 1987}
+                for term in terms:
+                    data[key][term] = 1
+            # go on to the next article
+            key = line.split()[1].strip()
+            text = ''
+            count += 1
+            if count % 10000 == 0:
+                print(count)
+        elif line.startswith('.U'):
+            field = None
+        elif line.startswith('.S'):
+            field = None
+        elif line.startswith('.M'):
+            field = 'terms'
+        elif line.startswith('.T'):
+            field = 'title'
+        elif line.startswith('.P'):
+            field = 'type'
+        elif line.startswith('.W'):
+            field = 'text'
+        elif line.startswith('.A'):
+            field = None
+        elif field == 'terms':
+            terms = line.strip().split(';')
+            terms = [term.strip() for term in terms]
+            for term in terms:
+                if term not in categories:
+                    categories[term] = len(categories)
+            if update_counts:
+                category_counts.update(terms)
+        elif field == 'title':
+            title = line.strip()
+        elif field == 'type':
+            type = line.strip()
+        elif field == 'text':
+            if text == '':
+                text = line.strip()
+            else:
+                text += '\n\n' + line.strip()
+
+    return data, categories, category_counts
 
 
 if __name__ == '__main__':
