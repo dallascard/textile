bc487259d65f7eb7582d472ba62cbe867742a965
dallascard <dcard@andrew.cmu.edu>
2017-11-07 16:13:51 -0800

    debug

--- core/experiments/plot_results_split_and_fit.py
+++ core/experiments/plot_results_split_and_fit.py
@@ -1,5 +1,6 @@
 from optparse import OptionParser
 
+import numpy as np
 import pandas as pd
 
 from ..util import file_handling as fh
@@ -16,23 +17,23 @@ def main():
 
     (options, args) = parser.parse_args()
     files = args
+    n_files = len(files)
 
     df = None
     values = None
-    n_files = 0
-    for f in files:
+    for f_i, f in enumerate(files):
         print(f)
         n_files += 1
         df_f = fh.read_csv_to_df(f)
         if values is None:
-            df = df_f
-            values = df['MAE'].values.copy()
-        else:
-            values += df_f['MAE'].values
-    print(values)
-    print(n_files)
+            n_rows, n_cols = df_f.shape
+            values = np.zeros([n_rows, n_files])
+        values[:, f_i] = df_f['MAE'].values
+
     values = values / float(n_files)
-    df = pd.DataFrame(values, columns=['MAE'], index=df.index)
+    df = pd.DataFrame(values, index=df.index)
+    print(df.mean(axis=1))
+    print(df.var(axis=1))
     print(df)
 
 
