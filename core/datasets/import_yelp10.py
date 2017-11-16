6d0ece679ce47ad201a092ec6fdaa3ec63b19e68
dallascard <dcard@andrew.cmu.edu>
2017-11-16 14:58:01 -0800

    started on yelp preprocessing

--- core/datasets/import_yelp10.py
+++ core/datasets/import_yelp10.py
@@ -8,7 +8,7 @@ from ..util import file_handling as fh
 
 
 def main():
-    usage = "%prog input_dir"
+    usage = "%prog input_dir project_dir"
     parser = OptionParser(usage=usage)
     #parser.add_option('--keyword', dest='key', default=None,
     #                  help='Keyword argument: default=%default')
@@ -18,20 +18,50 @@ def main():
     (options, args) = parser.parse_args()
 
     input_dir = args[0]
+    project_dir = args[1]
 
-    cities = Counter()
+    fh.makedirs(project_dir)
+
+    city_lookup = dict()
 
     lines = fh.read_json_lines(os.path.join(input_dir, 'business.json'))
     for key, line in lines.items():
         if key % 10000 == 0:
             print(key)
-        city = line['state']
-        cities.update([city])
+        city = line['city']
+        business_id = line['business_id']
+        city_lookup[business_id] = city
 
-    n = 50
-    most_common = cities.most_common(n)
-    for i in range(n):
-        print(most_common[i])
+    data = {}
+
+    lines = fh.read_json_lines(os.path.join(input_dir, 'review.json'))
+    toronto_count = 0
+    phoenix_count = 0
+    for key, line in lines.items():
+        if key % 10000 == 0:
+            print(key)
+        review_id = line['review_id']
+        text = line['text']
+        date = line['date']
+        year = date.split('-')[0]
+        funny = int(line['funny'])
+        useful = int(line['useful'])
+        cool = int(line['cool'])
+        business_id = line['business_id']
+        if business_id in city_lookup:
+            city = city_lookup[business_id]
+            if funny + useful + cool > 0:
+                if city == 'Phoenix':
+                    phoenix_count += 1
+                    data[review_id] = {'text': text, 'city': 1, 'date': date, 'year': year, 'labels': {'funny': {0: useful + cool, 1: funny}}}
+                elif city == 'Toronto':
+                    toronto_count += 1
+                    data[review_id] = {'text': text, 'city': 0, 'date': date, 'year': year, 'labels': {'funny': {0: useful + cool, 1: funny}}}
+            else:
+                print("%s not found in city lookup" % business_id)
+
+    print(toronto_count, phoenix_count)
+    fh.write_to_json(data, os.path.join(dirs.dir_data_raw(project_dir), 'all.json'))
 
 
 if __name__ == '__main__':
