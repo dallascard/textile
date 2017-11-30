2198c1e2d2f14b361720e368ea374ea2fa519669
dallascard <dcard@andrew.cmu.edu>
2017-11-30 14:54:13 -0800

    adding explicit seed

--- do_experiments_mfc.py
+++ do_experiments_mfc.py
@@ -55,8 +55,6 @@ def main():
     parser.add_option('--max_epochs', dest='max_epochs', default=200,
                       help='Maximum number of epochs for DAN training: default=%default')
 
-
-
     (options, args) = parser.parse_args()
     project = args[0]
     config = args[1]
@@ -97,9 +95,11 @@ def main():
 
     pairs = [('framing', 'Economic'), ('framing', 'Legality'), ('framing', 'Health'), ('framing', 'Political'), ('framing', 'Capacity'), ('framing', 'Crime')]
 
+    seed = 42
     for subset, label in pairs:
         print("\n\nStarting", subset, label)
-        over_time_split_and_fit.test_over_time(project, subset, config, model_type, 'year', train_start, train_end, first_year, last_year, n_train, n_calib, penalty, suffix, alpha_min=alpha_min, alpha_max=alpha_max, n_alphas=n_alphas, loss='log', objective=objective, do_ensemble=True, label=label, intercept=True, sample_labels=sample_labels, n_dev_folds=5, list_size=ls, repeats=repeats, lower=lower, cshift=cshift, dh=dh, init_lr=init_lr, dropout=dropout, patience=patience, max_epochs=max_epochs)
+        over_time_split_and_fit.test_over_time(project, subset, config, model_type, 'year', train_start, train_end, first_year, last_year, n_train, n_calib, penalty, suffix, seed=seed, alpha_min=alpha_min, alpha_max=alpha_max, n_alphas=n_alphas, loss='log', objective=objective, do_ensemble=True, label=label, intercept=True, sample_labels=sample_labels, n_dev_folds=5, list_size=ls, repeats=repeats, lower=lower, cshift=cshift, dh=dh, init_lr=init_lr, dropout=dropout, patience=patience, max_epochs=max_epochs)
+        seed +=1
 
 
 if __name__ == '__main__':
