2a511cb6f2a5eb2e621fa954e64eb0a5fdcf69cd
dallascard <dcard@andrew.cmu.edu>
2017-12-02 13:09:20 -0800

    want to try dans on amazon

--- core/util/generate_amazon_scripts.py
+++ core/util/generate_amazon_scripts.py
@@ -18,36 +18,39 @@ def main():
     labels = ['helpfulness', 'fivestar']
     n_train_vals = [500, 5000]
     use_cshift_options = [False, True]
-    objectives = ['f1', 'calibration']
+    objectives = ['f1', 'calibration', 'acc']
+    model_types = ['LR', 'DAN']
 
     seed = 42
-    for objective in objectives:
-        for use_cshift in use_cshift_options:
-            for subset in subsets:
-                for label in labels:
-                    for n_train in n_train_vals:
-                        cmd = 'python -m core.experiments.over_time_split_and_fit'
-                        cmd += ' projects/amazon/' + subset
-                        cmd += ' all config/n5grams.json'
-                        cmd += ' --label ' + label
-                        cmd += ' --test_start 2013'
-                        cmd += ' --test_end 2014'
-                        cmd += ' --n_train ' + str(n_train)
-                        cmd += ' --sample'
-                        if n_train == 500:
-                            cmd += ' --repeats 10'
-                        else:
-                            cmd += ' --repeats 5'
-                        cmd += ' --objective ' + objective
-                        if use_cshift:
-                            cmd += ' --cshift'
-                        cmd += ' --seed ' + str(seed)
+    for model in model_types:
+        for objective in objectives:
+            for use_cshift in use_cshift_options:
+                for subset in subsets:
+                    for label in labels:
+                        for n_train in n_train_vals:
+                            cmd = 'python -m core.experiments.over_time_split_and_fit'
+                            cmd += ' projects/amazon/' + subset
+                            cmd += ' all config/n5grams.json'
+                            cmd += ' --model ' + model
+                            cmd += ' --label ' + label
+                            cmd += ' --test_start 2013'
+                            cmd += ' --test_end 2014'
+                            cmd += ' --n_train ' + str(n_train)
+                            cmd += ' --sample'
+                            if n_train == 500:
+                                cmd += ' --repeats 10'
+                            else:
+                                cmd += ' --repeats 5'
+                            cmd += ' --objective ' + objective
+                            if use_cshift:
+                                cmd += ' --cshift'
+                            cmd += ' --seed ' + str(seed)
 
-                        name = '_'.join(['run', subset, label, str(n_train), objective, 'cshift', str(use_cshift)])
-                        script = make_script(name, [cmd], 1, 16, 10, False, 'pytorch', [])
+                            name = '_'.join(['run', subset, label, str(n_train), objective, 'cshift', str(use_cshift)])
+                            script = make_script(name, [cmd], 1, 16, 10, False, 'pytorch', [])
 
-                        with open(name + '.sh', 'w') as f:
-                            f.write(script)
+                            with open(name + '.sh', 'w') as f:
+                                f.write(script)
 
 
 
