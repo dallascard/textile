140755b5dbdb54ca83f70cc7c81d734e8abb2e13
dallascard <dcard@andrew.cmu.edu>
2017-12-08 17:05:55 -0800

    testing new plotting script

---
+++ core/plotting/plot_amazon_series.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import glob
+from optparse import OptionParser
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+def main():
+    usage = "%prog"
+    parser = OptionParser(usage=usage)
+    #parser.add_option('--keyword', dest='key', default=None,
+    #                  help='Keyword argument: default=%default')
+    #parser.add_option('--boolarg', action="store_true", dest="boolarg", default=False,
+    #                  help='Keyword argument: default=%default')
+
+
+    (options, args) = parser.parse_args()
+
+    input_dir = 'projects/amazon/sports5/models/'
+    train_vals = []
+    pcc_f1_vals = []
+    acc_f1_vals = []
+    cal_f1_vals = []
+    platt_vals = []
+    acc_vals = []
+    cshift_vals = []
+
+    years = [2009, 2010, 2011, 2012, 2013, 2014]
+    for year in ['2009', '2010', '2011', '2012', '2013', '2014']:
+        acc_file = os.path.join(input_dir, 'all_helpfulness_l1_acc_2000_0_sampled_' + year + '-' + year + '_0', 'results.csv')
+        cal_file = os.path.join(input_dir, 'all_helpfulness_l1_calibration_2000_0_sampled_' + year + '-' + year + '_0', 'results.csv')
+        f1_file = os.path.join(input_dir, 'all_helpfulness_l1_f1_2000_0_sampled_' + year + '-' + year + '_0', 'results.csv')
+        #cshift_file = os.path.join(input_dir, 'all_helpfulness_l1_f1_2000_0_sampled_cshift_' + year + '-' + year + '_0', 'results.csv')
+
+        acc_df = pd.read_csv(acc_file, index_col=0, header=0)
+        cal_df = pd.read_csv(cal_file, index_col=0, header=0)
+        f1_df = pd.read_csv(f1_file, index_col=0, header=0)
+        #cshift_df = pd.read_csv(cshift_file, index_col=0, header=0)
+
+        train_vals.append(f1_df.loc['train', 'RMSE'])
+
+    fig, ax = plt.subplots()
+    ax.plot(years, train_vals)
+    plt.savefig('test.pdf')
+
+
+if __name__ == '__main__':
+    main()
