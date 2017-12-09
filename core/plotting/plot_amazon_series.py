277d9f066b3293f4c0617e12c480eca6438a723a
dallascard <dcard@andrew.cmu.edu>
2017-12-08 17:25:13 -0800

    testing new plotting script

--- core/plotting/plot_amazon_series.py
+++ core/plotting/plot_amazon_series.py
@@ -29,20 +29,20 @@ def main():
     pcc_acc_vals = []
     pcc_cal_vals = []
     platt_vals = []
-    acc_vals = []
+    #acc_vals = []
     cshift_vals = []
 
-    years = [2009, 2010, 2011, 2012, 2013, 2014]
-    for year in ['2009', '2010', '2011', '2012', '2013', '2014']:
+    years = [2009, 2010, 2011, 2012]
+    for year in ['2009', '2010', '2011', '2012']:
         acc_file = os.path.join(input_dir, 'all_helpfulness_l1_acc_2000_0_sampled_' + year + '-' + year + '_0', 'results.csv')
         cal_file = os.path.join(input_dir, 'all_helpfulness_l1_calibration_2000_0_sampled_' + year + '-' + year + '_0', 'results.csv')
         f1_file = os.path.join(input_dir, 'all_helpfulness_l1_f1_2000_0_sampled_' + year + '-' + year + '_0', 'results.csv')
-        #cshift_file = os.path.join(input_dir, 'all_helpfulness_l1_f1_2000_0_sampled_cshift_' + year + '-' + year + '_0', 'results.csv')
+        cshift_file = os.path.join(input_dir, 'all_helpfulness_l1_f1_2000_0_sampled_cshift_' + year + '-' + year + '_0', 'results.csv')
 
         acc_df = pd.read_csv(acc_file, index_col=0, header=0)
         cal_df = pd.read_csv(cal_file, index_col=0, header=0)
         f1_df = pd.read_csv(f1_file, index_col=0, header=0)
-        #cshift_df = pd.read_csv(cshift_file, index_col=0, header=0)
+        cshift_df = pd.read_csv(cshift_file, index_col=0, header=0)
 
         target_vals.append(f1_df['estimate'].loc['target'])
         target_p = f1_df['estimate'].loc['target']
@@ -51,9 +51,10 @@ def main():
         train_vals.append(f1_df['estimate'].loc['train'])
         pcc_f1_vals.append(f1_df['estimate'].loc['PCC'])
         platt_vals.append(f1_df['estimate'].loc['PCC_platt2'])
-        acc_vals.append(f1_df['estimate'].loc['ACC_internal'])
+        #acc_vals.append(f1_df['estimate'].loc['ACC_internal'])
         pcc_acc_vals.append(acc_df['estimate'].loc['PCC'])
         pcc_cal_vals.append(cal_df['estimate'].loc['PCC'])
+        cshift_vals.append(cshift_df['estimate'].loc['PCC'])
 
     fig, ax = plt.subplots()
     ax.plot(years, target_vals, label='Target')
@@ -64,7 +65,8 @@ def main():
     ax.plot(years, pcc_acc_vals, label='PCC(acc)')
     ax.plot(years, pcc_f1_vals, label='PCC(F1)')
     ax.plot(years, platt_vals, label='Platt')
-    ax.plot(years, acc_vals, label='ACC')
+    ax.plot(years, cshift_vals, label='Reweighting')
+    #ax.plot(years, acc_vals, label='ACC')
     ax.legend()
     plt.savefig('test.pdf')
 
