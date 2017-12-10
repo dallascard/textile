d04ef16dc6ec00b1225136c849e761b150a22a29
dallascard <dcard@andrew.cmu.edu>
2017-12-09 19:23:29 -0800

    plotting

--- core/plotting/make_twitter_plot.py
+++ core/plotting/make_twitter_plot.py
@@ -60,7 +60,7 @@ def main():
         ax.scatter(x, ACC_vals, s=10, color='b', alpha=0.6, label=labels[0])
         ax.scatter(x, PCC_vals, s=10, color='g', marker='x', alpha=0.6, label=labels[1])
 
-    ax.legend(loc='top')
+    ax.legend(loc='upper center')
     ax.set_xlabel('Target proportions')
     ax.set_ylabel('MAE')
 
