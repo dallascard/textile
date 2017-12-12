ac57c720920af1aacaa91bc9857616fefd020ecb
dallascard <dcard@andrew.cmu.edu>
2017-12-11 16:04:09 -0800

    plotting

--- core/plotting/make_twitter_plot.py
+++ core/plotting/make_twitter_plot.py
@@ -32,7 +32,7 @@ def main():
         CC_values[t] = []
 
     for t in targets:
-        files = glob(os.path.join('projects', 'twitter', 'models', 'train_positive_l1_f1_5000_0' + t + '_167-167_*', 'results.csv'))
+        files = glob(os.path.join('projects', 'twitter', 'models', 'train_positive_l1_f1_5000_0' + t + '_166-166_*', 'results.csv'))
 
         for f in files:
             print(f)
