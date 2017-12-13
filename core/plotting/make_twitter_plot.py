e4b0531d117f255fe67246b555baeca3dc55cd2d
dallascard <dcard@andrew.cmu.edu>
2017-12-12 22:19:52 -0800

    plotting

--- core/plotting/make_twitter_plot.py
+++ core/plotting/make_twitter_plot.py
@@ -70,14 +70,13 @@ def main():
 
 
     ax.legend(loc='upper center')
-    ax.set_xlabel('Difference between source and target proportions')
+    ax.set_xlabel('Modified target label proportion')
     ax.set_ylabel('MAE')
     ax.set_ylim(0, 0.15)
     #x = [float(t) for t in targets]
-    x_vals = [0.5, 0.55, 0.6, 0.65, 0.7]
-    labels = ['-0.1', '-0.05', '0', '0.05', '0.1']
-    ax.set_xticks(x_vals)
-    ax.set_xticklabels(labels)
+    #x_vals = [0.5, 0.55, 0.6, 0.65, 0.7]
+    #ax.set_xticks(x_vals)
+    #ax.set_xticklabels(labels)
     plt.savefig(output + '.pdf', bbox_inches='tight')
 
 
